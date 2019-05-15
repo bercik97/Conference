b1a2fcf35b0c1185b9786cc89c80f21d600a4e9b
RoberKrzywina <robert.krzywina@gmail.com>
2019-05-15 20:44:28 +0200

    Add spock and basic config

---
+++ src/test/groovy/pl.robert.conference/ConferenceApplicationSpec.groovy
@@ -0,0 +1,18 @@
+package pl.robert.conference
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import spock.lang.Specification
+
+@SpringBootTest
+class ConferenceApplicationSpec extends Specification {
+
+    @Autowired
+    ApplicationContext context
+
+    def 'Should load Spring context'() {
+        expect:
+        context != null
+    }
+}
