df013e5baea26b8488ec93ad5dfb524ab5ed80fb
RoberKrzywina <robert.krzywina@gmail.com>
2019-05-20 13:28:02 +0200

    Change packages and add conference facade test

---
+++ src/test/groovy/pl/robert/app/conference/domain/ConferenceSpec.groovy
@@ -0,0 +1,17 @@
+package pl.robert.app.conference.domain
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ConferenceSpec extends Specification {
+
+    @Autowired
+    ConferenceFacade facade
+
+    def 'Should find any conference'()  {
+        expect:
+        facade.find() != null
+    }
+}
