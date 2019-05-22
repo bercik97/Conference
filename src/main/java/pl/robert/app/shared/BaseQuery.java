e938b2585aaa4f9b7b101de78d0a85e6c595da98
RoberKrzywina <robert.krzywina@gmail.com>
2019-05-22 20:04:03 +0200

    Add abstract class BaseQuery and modify BaseEntity class

--- src/main/java/pl/robert/app/shared/BaseQuery.java
+++ src/main/java/pl/robert/app/shared/BaseQuery.java
@@ -2,21 +2,29 @@ package pl.robert.app.shared;
 
 import lombok.Getter;
 import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import java.util.UUID;
 import java.util.Objects;
 
-import java.io.Serializable;
-
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @MappedSuperclass
-public abstract class BaseEntity implements Serializable {
+@NoArgsConstructor
+public abstract class BaseQuery {
 
-    String uuid = UUID.randomUUID().toString();
+    @Id
+    Long id;
+
+    String uuid;
+
+    public BaseQuery(Long id, String uuid) {
+        this.id = id;
+        this.uuid = uuid;
+    }
 
     @Override
     public int hashCode() {
@@ -25,7 +33,7 @@ public abstract class BaseEntity implements Serializable {
 
     @Override
     public boolean equals(Object that) {
-        return this == that || that instanceof BaseEntity
-                    && Objects.equals(uuid, ((BaseEntity) that).uuid);
+        return this == that || that instanceof BaseQuery
+                    && Objects.equals(uuid, ((BaseQuery) that).uuid);
     }
 }
